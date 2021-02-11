23db7797a3df308b83776dc5fff04672c831346e
Massimiliano Fadda <72360779+massi-fa@users.noreply.github.com>
2021-02-11 21:42:07 +0100

    link add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
+import styled from "styled-components/macro";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+
 import Login from "./components/Login";
 import Register from "./components/Register";
 
+const Container = styled.div`
+
+`;
+
 const App = () => ( 
-  <div>
-    <Login />
-  </div>
+  <Router>
+    <Switch>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/register">
+        <Register />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default App;
