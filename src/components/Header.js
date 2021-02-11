02480bd2da1a43f8ca61c2a3f73730e94d7a524e
Massimiliano Fadda <72360779+massi-fa@users.noreply.github.com>
2021-02-11 23:06:04 +0100

    header start

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components/macro";
+
+const Container = styled.div`
+
+`;
+
+const Header = () =>{
+  return(
+		<Container>
+			<h1>Ciaoo</h1>
+		</Container>
+	);
+}
