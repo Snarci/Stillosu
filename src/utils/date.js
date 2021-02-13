f01d4e30a486a0ea73043f8d3850f034c3116579
Snarci <58110072+Snarci@users.noreply.github.com>
2021-02-13 14:41:18 +0100

    Update dati vestiti

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,33 +1,117 @@
 import search from "../res/pp.jpg";
+
+//Immagini da usare
+
+//Sezione t-shirt
+import T1 from "../res/ImmaginiAbiti/T-shirt/1P.png";
+import T2 from "../res/ImmaginiAbiti/T-shirt/2P.png";
+import T3 from "../res/ImmaginiAbiti/T-shirt/3P.png";
+import T4 from "../res/ImmaginiAbiti/T-shirt/4P.png";
+//Sezione maglioni
+import M1 from "../res/ImmaginiAbiti/Maglioni/1P.png";
+import M2 from "../res/ImmaginiAbiti/Maglioni/2P.png";
+import M3 from "../res/ImmaginiAbiti/Maglioni/3P.png";
+import M4 from "../res/ImmaginiAbiti/Maglioni/4P.png";
+//Sezione pantaloni
+import P1 from "../res/ImmaginiAbiti/Pantaloni/1P.png";
+import P2 from "../res/ImmaginiAbiti/Pantaloni/2P.png";
+import P3 from "../res/ImmaginiAbiti/Pantaloni/3P.png";
+import P4 from "../res/ImmaginiAbiti/Pantaloni/4P.png";
+//Sezione scarpe
+import S1 from "../res/ImmaginiAbiti/Scarpe/1P.png";
+import S2 from "../res/ImmaginiAbiti/Scarpe/2P.png";
+import S3 from "../res/ImmaginiAbiti/Scarpe/3P.png";
+import S4 from "../res/ImmaginiAbiti/Scarpe/4P.png";
+//Sezione cappelli
+import C1 from "../res/ImmaginiAbiti/Cappelli/1P.png";
+import C2 from "../res/ImmaginiAbiti/Cappelli/2P.png";
+import C3 from "../res/ImmaginiAbiti/Cappelli/3P.png";
+import C4 from "../res/ImmaginiAbiti/Cappelli/4P.png";
+
+//sCARPE CAPPELLO TOP BOT
 import React from 'react';
 
 export const abiti = [
   {
     "nome": "capello",
-    "immagine": search
+    "immagine": T1
   },
   {
     "nome": "capello",
-    "immagine": search
+    "immagine": T2
   },
   {
     "nome": "capello",
-    "immagine": search
+    "immagine": T3
   },
   {
     "nome": "giacca",
-    "immagine": search
+    "immagine": T4
   },
   {
-    "nome": "maglietta",
-    "immagine": search
+    "nome": "capello",
+    "immagine": M1
   },
   {
-    "nome": "maglietta",
-    "immagine": search
+    "nome": "capello",
+    "immagine": M2
   },
   {
-    "nome": "calzino",
-    "immagine": search
+    "nome": "capello",
+    "immagine": M3
+  },
+  {
+    "nome": "giacca",
+    "immagine": M4
+  },
+  {
+    "nome": "capello",
+    "immagine": P1
+  },
+  {
+    "nome": "capello",
+    "immagine": P2
+  },
+  {
+    "nome": "capello",
+    "immagine": P3
+  },
+  {
+    "nome": "giacca",
+    "immagine": P4
+  }
+  ,
+  {
+    "nome": "capello",
+    "immagine": S1
+  },
+  {
+    "nome": "capello",
+    "immagine": S2
+  },
+  {
+    "nome": "capello",
+    "immagine": S3
+  },
+  {
+    "nome": "giacca",
+    "immagine": S4
+  }
+  ,
+  {
+    "nome": "capello",
+    "immagine": C1
+  },
+  {
+    "nome": "capello",
+    "immagine": C2
+  },
+  {
+    "nome": "capello",
+    "immagine": C3
+  },
+  {
+    "nome": "giacca",
+    "immagine": C4
   }
 ];
